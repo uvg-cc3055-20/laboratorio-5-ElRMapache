e8a075769393b55a90bc9278ee5fff5490aa25e3
ElRMapache <alo15470@uvg.edu.gt>
2018-02-15 22:05:43 -0600

    AVANCE
    casi todo el lab esta listo, faltan 2 scripts

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+
+    public float time;
+    public IEnumerator coroutine;
+
+	// Use this for initialization
+	void Start () {
+      //  coroutine;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = Time.deltaTime;
+      
+	}
+}
